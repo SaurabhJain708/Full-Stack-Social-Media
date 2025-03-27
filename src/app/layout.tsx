c981aafadad48d703ee325495547e0749f9a3331
SaurabhJain708 <saurabhanchal381@gmail.com>
2025-03-27 17:44:23 +0530

    Fixed redux errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { store } from "./redux/store";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
+import { Providers } from "./redux/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Provider store={store}>{children}</Provider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
